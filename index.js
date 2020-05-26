f8528037a970f2ac4d9c1a38ec3831597f129ba5
zacfinger <zac@zacfinger.com>
2020-05-25 18:51:50 -0700

    Now loops through first 30 results in query and displays title

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const axios = require('axios');
 
 (async () => {
   try {
-    const response = await axios.get('https://hacker-news.firebaseio.com/v0/item/23304442.json?print=pretty')
-    console.log(response.data.url);
-    console.log(response.data);
+    const response = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    
+    for(var post=0; post < 30; post++){
+        
+        (async () => {
+            try {
+                const story = await axios.get('https://hacker-news.firebaseio.com/v0/item/' + response.data[post] + '.json?print=pretty')
+                console.log(story.data.title);
+            }
+            catch(error){
+                console.log("Nested async error: " + error);
+            }
+        })();
+        
+    }
+
   } catch (error) {
-    console.log(error.response.body);
+    console.log(error + " something error happens");
   }
 })();
