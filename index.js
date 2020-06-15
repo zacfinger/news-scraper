eabea486af64ec8e0aeb0088fdf0eb7b5013feea
zacfinger <zac@zacfinger.com>
2020-06-14 20:52:28 -0700

    Now saving links from HN in firebase data storage

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const newsScraperCredentials = require('./news-scraper-credentials');
 const fs = require('fs');
 const axios = require('axios');
+const admin = require('firebase-admin');
+
+let serviceAccount = require(newsScraperCredentials.jsonPath);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
 
 // Get sites
 // https://stackabuse.com/reading-and-writing-json-files-with-node-js/
+// need to check if JSON file exists and create it if not
 let rawdata = fs.readFileSync('sites.json');
 let sites = JSON.parse(rawdata);
 
@@ -30,6 +41,7 @@ function getDomain(url){
   return false;
 }
 
+
 (async () => {
   try {
     const response = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
@@ -43,6 +55,14 @@ function getDomain(url){
                 //console.log(story.data.title);
                 if (story.data.url != undefined){
                     if(getDomain(story.data.url)){
+                      
+                      let docRef = db.collection('links').doc();
+
+                      let setAda = docRef.set({
+                        title: story.data.title,
+                        url: story.data.url
+                      });
+
                       console.log(story.data.title);
                     }
                 }
--- scraper.js
+++ scraper.js
@@ -1,4 +1,5 @@
 // https://levelup.gitconnected.com/web-scraping-with-node-js-c93dcf76fe2b
+// https://blog.bitsrc.io/https-blog-bitsrc-io-how-to-perform-web-scraping-using-node-js-5a96203cb7cb
 // https://stackoverflow.com/questions/49432579/await-is-only-valid-in-async-function
 
 const siteUrl = "https://www.alexa.com/topsites/category/Top/News";
@@ -23,6 +24,8 @@ const getResults = async () => {
     return sites;
 };
 
+// Eventually need to save data in FireBase DB
+// https://firebase.google.com/docs/database/admin/save-data
 (async () => {
     let results = await getResults()
     let jsonString = JSON.stringify(results);
