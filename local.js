5e6207a4ba7881acc6ce7559a025c0458b1bdac3
zacfinger <zac@zacfinger.com>
2020-12-12 12:19:55 -0700

    Cleaned up conditional dependency inclusion

--- local.js
+++ local.js
@@ -1,28 +1,39 @@
 // Pulls news items from Google News RSS feed
 // Stores data in database
 
-// Require dependencies
+// Load config settings
+const config = require('./config');
+let useSQL = config.useSQL; // Boolean: use SQL or Firestore for db
+
+// Require RSS dependencies
 let Parser = require('rss-parser');
 let parser = new Parser();
-var mysql = require('./dbcon.js');
-const config = require('./config');
-const admin = require('firebase-admin');
-let serviceAccount = require(config.jsonPath);
 
-// Boolean config value to use SQL or Firestore
-let useSQL = config.useSQL;
+// Instantiate dependency objects
+// Later load conditionally based on config
+let mysql = null; // mysql object
 
-// Get Firestore values if needed
+// Firestore objects
+let admin = null;
+let serviceAccount = null;
 let db = null;
-if (!useSQL)
-{
-	admin.initializeApp({
-  		credential: admin.credential.cert(serviceAccount)
-	});
 
-	db = admin.firestore();
+// Conditionally load dependencies
+if (useSQL) {
+    mysql = require('./dbcon.js');
+} else {
+    admin = require('firebase-admin');
+    serviceAccount = require(config.jsonPath);
+
+    // Get Firestore values if needed
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
+    db = admin.firestore();
 }
 
+// Main
 (async() => {
 
     // Get Date.now and subtract one hour
@@ -39,38 +50,34 @@ if (!useSQL)
         // If the news item was published within last 60 minutes
         if (date >= oneHourAgo) {
 		
-		// TODO: Check if item.link contains word in denylist
-		(async() => {
-			if(useSQL)
-			{
-				// TODO: Check guid does not exceed the column limit
-				// TODO: Check guid does not exist
-				mysql.pool.query(
-					'insert into linksToProcess (`guid`, `link`, `pubDate`) values (?, ?, ?)',
-					[item.guid, item.link, date],
-					(err, result) => {
-						if(err)
-						{
-							console.log(err);
-						}
-						else
-						{
-							console.log(result);
-						}
-					}
-				);
-			}
-			else
-			{
-				// Populate to firestore db
-				db.collection('linksToProcess').doc(item.guid).set({
-					url: item.link,
-					time: date
-				});
-			}
-		})();
-
+            // TODO: Check if item.link contains word in denylist
+            (async() => {
+                if(useSQL) {
+                    // TODO: Check guid does not exceed the column limit
+                    // TODO: Check guid does not exist
+                    mysql.pool.query(
+                        'insert into linksToProcess (`guid`, `link`, `pubDate`) values (?, ?, ?)',
+                        [item.guid, item.link, date],
+                        (err, result) => {
+                            if(err)
+                            {
+                                console.log(err);
+                            }
+                            else
+                            {
+                                console.log(result);
+                            }
+                        }
+                    );
+                }
+                else {
+                    // Populate to firestore db
+                    db.collection('linksToProcess').doc(item.guid).set({
+                        url: item.link,
+                        time: date
+                    });
+                }
+            })();
         }
-
     });
 })();
